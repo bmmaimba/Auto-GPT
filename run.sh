44c90503ec1a59c3b5fe153cb9bd593af4a70929
benjamin@strategicdimensions.co.za <benjamin@strategicdimensions.co.za>
2023-04-25 12:17:29 +0200

    Use python3.10
    Use python3.10

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ python scripts/check_requirements.py requirements.txt
 if [ $? -eq 1 ]
 then
     echo Installing missing packages...
-    pip install -r requirements.txt
+   #pip install -r requirements.txt
+    python3.10 -m pip install -r requirements.txt
 fi
-python -m autogpt $@
+python3.10 -m autogpt $@
 read -p "Press any key to continue..."
